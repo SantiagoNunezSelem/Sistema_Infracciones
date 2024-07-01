fcd906a5088b84acf4567ebffcb67f4d02e5eebc
Santiago N.S. <gigogisantisd@gmail.com>
2024-06-30 21:25:11 -0300

    Get datos Vehiculo

--- CapaNegocio/Vehiculo.cs
+++ CapaNegocio/Vehiculo.cs
@@ -6,17 +6,37 @@ using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class Vehiculo
+    public class Vehiculo
     {
-        private int dominio;
-        private int dniPropietario;
+        private string dominio;
+        private string dniPropietario;
         private string modelo;
 
-        public Vehiculo(int dominio, int dniPropietario, string modelo)
+        public Vehiculo(string dominio, string dniPropietario, string modelo)
         {
             this.dominio = dominio;
             this.dniPropietario = dniPropietario;
             this.modelo = modelo;
+        }
+
+        public bool esVehiculo(string dominio)
+        {
+            return (this.dominio.Equals(dominio));
+        }
+
+        public string Dominio
+        {
+            get { return dominio; }
+        }
+
+        public string DniPropietario
+        {
+            get { return dniPropietario; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
         }
     }
 }
