1186c4eb22f5a0b5cbe12567618d778b8a28250d
Santiago N.S. <gigogisantisd@gmail.com>
2024-06-30 21:23:35 -0300

    Lista Vehiculos e Infracciones. Metodos para buscarlos

--- CapaNegocio/Administradora.cs
+++ CapaNegocio/Administradora.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,18 @@ namespace CapaNegocio
     {
         private static Administradora instancia;
         private List<Infraccion> infracciones;
+        private List<Vehiculo> vehiculos;
 
         public Administradora()
         {
             infracciones = new List<Infraccion>();
-
+            vehiculos = new List<Vehiculo>();
             //Hardcodeado
-            InfraccionGrave infra = new InfraccionGrave(1,"Mal Estacionamiento","soy una descripcion",5000);
+            InfraccionGrave infra = new InfraccionGrave("1","Mal Estacionamiento","soy una descripcion",5000);
             infracciones.Add(infra);
+
+            Vehiculo vehiculo = new Vehiculo("111","11111111","modelo1");
+            vehiculos.Add(vehiculo);
         }
 
         public static Administradora getInstanciaAdministradora()
@@ -34,6 +39,20 @@ namespace CapaNegocio
         public Infraccion getInfraccion()
         {
             return infracciones.FirstOrDefault();
+        }
+
+        public Vehiculo getVehiculo(string dominio)
+        {
+            Vehiculo buscarVehiculo;
+            buscarVehiculo = vehiculos.Find(v => v.esVehiculo(dominio));
+            return buscarVehiculo;
+        }
+
+        public Infraccion getInfraccion(string codigo)
+        {
+            Infraccion infraccion;
+            infraccion = infracciones.Find(i => i.esInfraccion(codigo));
+            return infraccion;
         }
 
         static void Main()
