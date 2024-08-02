af3569f729df4d76bc8b98e61e90b550f703b051
Santiago N.S. <gigogisantisd@gmail.com>
2024-08-02 11:59:42 -0300

    Agregar DbContext y modelos de entityFramework, recuperar vehiculos de DB

--- CapaDatos/Datos.cs
+++ CapaDatos/Datos.cs
@@ -4,13 +4,37 @@ using System.Collections;
 using System.Data;
 using System.Data.OleDb;
 using System.Runtime.CompilerServices;
-
+using System.Data.Entity;
+using Capa_de_Datos;
+using System.Linq;
 
 namespace Capa_Datos
 {
-    public static class Datos
+    public class Datos : DbContext
     {
+        public Datos()
+            : base("name=SistemaInfraccionesDBEntities")
+        {
+        }
 
+        public DbSet<Infraccion> Infracciones {  get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<PagoInfraccion> PagoInfracciones { get; set; }
+
+        public static List<Vehiculo> recuperarVehiculos()
+        {
+            try
+            {
+                using (var context = new Datos())
+                {
+                    return context.Vehiculos.ToList();
+                }
+            }
+            catch{
+                return null;
+            }
+            
+        }
     }
 }
 
